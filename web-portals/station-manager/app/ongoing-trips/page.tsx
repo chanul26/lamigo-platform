0a04385a1782ad6e25b7bed955fb12f2df29d20e
LaksayanUthayakumaran <laksayan876@gmail.com>
2026-03-11 10:18:13 +0530

    Change item() to map() for filteredTrips

--- web-portals/station-manager/app/ongoing-trips/page.tsx
+++ web-portals/station-manager/app/ongoing-trips/page.tsx
@@ -98,7 +98,7 @@ export default function OngoingTripsPage() {
 
       {/* Ongoing Trips Cards */}
       <div className="grid grid-cols-1 gap-6">
-        {filteredTrips.item((trip) => (
+        {filteredTrips.map((trip) => (
           <Link
             key={trip.id}
             href={`/ongoing-trips/${trip.id}`}
