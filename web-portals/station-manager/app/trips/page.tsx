39bc6c109fb27b36c8d89eefa951fca56aa2b5dd
LaksayanUthayakumaran <laksayan876@gmail.com>
2026-03-11 10:47:56 +0530

    Update trip status from 'Progress' to 'In Progress'

--- web-portals/station-manager/app/trips/page.tsx
+++ web-portals/station-manager/app/trips/page.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 
 interface Trip {
   id: string
-  status: 'Progress' | 'Draft' | 'Completed'
+  status: 'In Progress' | 'Draft' | 'Completed'
   driver: string 
   driverInitials: string
   packageCount: number
